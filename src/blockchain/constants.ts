b55c979c6ee0bcf8c6b5e1188c693bb4da4322e1
chef1119 <cheft1119@gmail.com>
2022-10-27 22:20:59 +0800

    initial commit

---
+++ src/blockchain/constants.ts
@@ -0,0 +1,37 @@
+export const defaultChainId = 97;
+
+export const rpcUrls = {
+  1337: 'http://localhost:8545',
+  56: 'https://bsc-dataseed1.binance.org',
+  97: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+  1: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161/',
+}
+
+export const networkNames = {
+  // 1: 'Ethereum Mainnet',
+  1337: 'Localhost8545',
+  56: 'BSC Main Network',
+  97: 'BSC Test Network',
+  1: 'Ethereum Mainnet'
+}
+
+export const addresses = {
+  coinracerNFT: {
+    97: '0x8388EBd5D5DDB321d4e2673af1dfDD382F9141a5',
+    56: '0x1B967351e96Bc52E7f4c28EB97406bfa7eB8c8b2'
+  },
+  coinracerToken: {
+    97: '0xe9852a19b7E15993d99a51099eb3f8DAC4f51997',
+    56: '0xfbb4f2f342c6daab63ab85b0226716c4d1e26f36'
+  },
+  busdToken: {
+    97: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
+    56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
+  },
+  coinStreamer: {
+    97: '0x7CAB7053ca06a485911CFC50Ff9D3818832D87c1'
+  }
+}
+
+
+export const decimal = 8;
