eed0a752dbd687c51e5f549a62c3ebe8b854068d
chef1119 <cheft1119@gmail.com>
2022-10-27 22:30:40 +0800

    meta fix

--- src/blockchain/constants.ts
+++ src/blockchain/constants.ts
@@ -16,18 +16,6 @@ export const networkNames = {
 }
 
 export const addresses = {
-  coinracerNFT: {
-    97: '0x8388EBd5D5DDB321d4e2673af1dfDD382F9141a5',
-    56: '0x1B967351e96Bc52E7f4c28EB97406bfa7eB8c8b2'
-  },
-  coinracerToken: {
-    97: '0xe9852a19b7E15993d99a51099eb3f8DAC4f51997',
-    56: '0xfbb4f2f342c6daab63ab85b0226716c4d1e26f36'
-  },
-  busdToken: {
-    97: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
-    56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
-  },
   coinStreamer: {
     97: '0x7CAB7053ca06a485911CFC50Ff9D3818832D87c1'
   }
